dfe2b3cfc698063f92e17244341bd7ada45fef42
3seizero <carlo@3seizero.com>
2026-07-02 12:48:52 +0200

    Icone in root con nomi nuovi (deploy affidabile + cache-bust iOS)

--- app-src/src/config/project.js
+++ app-src/src/config/project.js
@@ -30,7 +30,7 @@ export const project = {
     hero: '', // hero su verde pieno (foto hero dedicata: in attesa da Carlo)
     logosFooter: '', // striscia loghi footer rimossa (richiesta Carlo)
     favicon: '/favicon-96x96.png',
-    icon: '/icons/icon-192.png',
+    icon: '/appicon-192.png',
   },
 
   form: {
